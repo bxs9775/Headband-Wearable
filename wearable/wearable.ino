d78ee27070aeef5a8820b40e397af1dd6005df9a
bxs9775 <bxs9775@rit.edu>
2018-03-15 15:28:48 -0400

    v.1 headband with vibrating motor

---
+++ wearable/wearable.ino
@@ -0,0 +1,113 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+
+/*Code based of accelsensor demo - https://github.com/adafruit/Adafruit_LSM303DLHC/blob/master/examples/accelsensor/accelsensor.pde
+
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+
+sensors_event_t prevEvent;
+
+int motorPin = 2;
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  accel.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+void setup(void)
+{
+#ifndef ESP8266
+  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
+#endif
+  Serial.begin(9600);
+  Serial.println("Accelerometer Test"); Serial.println("");
+
+  /* Initialise the sensor */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+
+  pinMode(motorPin,OUTPUT);
+
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+}
+
+void loop(void)
+{
+  /* Get a new sensor event */
+  sensors_event_t event;
+  accel.getEvent(&event);
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  //Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("X: "); Serial.print(prevEvent.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(prevEvent.acceleration.y); Serial.print("  ");
+  //Serial.print("Z: "); Serial.print(prevEvent.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+
+  /*Check changing...*/
+  bool xChange = abs(event.acceleration.x-prevEvent.acceleration.x) > 1.0;
+  bool yChange = abs(event.acceleration.y-prevEvent.acceleration.y) > 1.0;
+ // bool zChange = abs(event.acceleration.z-prevEvent.acceleration.z) > 1.0;
+  Serial.print("X: ");
+  if( xChange ){
+    Serial.print("Yes  ");
+  } else {
+    Serial.print("No   ");
+  }
+  Serial.print("Y: ");
+  if( yChange ){
+    Serial.print("Yes  ");
+  } else {
+    Serial.print("No   ");
+  }
+  /*
+  Serial.print("Z: ");
+  if( zChange ){
+    Serial.print("Yes  ");
+  } else {
+    Serial.print("No   ");
+  }
+  */
+  Serial.print(">=2 axises: ");
+  if(xChange && yChange){
+    Serial.print("Yes  ");
+    digitalWrite(motorPin,HIGH);
+  } else {
+    Serial.print("No  ");
+    digitalWrite(motorPin,LOW);
+  }
+  Serial.println("");
+
+  /* Note: You can also get the raw (non unified values) for */
+  /* the last data sample as follows. The .getEvent call populates */
+  /* the raw values used below. */
+  //Serial.print("X Raw: "); Serial.print(accel.raw.x); Serial.print("  ");
+  //Serial.print("Y Raw: "); Serial.print(accel.raw.y); Serial.print("  ");
+  //Serial.print("Z Raw: "); Serial.print(accel.raw.z); Serial.println("");
+
+  /*Set the prevEvent to the current*/
+  prevEvent = event;
+  /* Delay before the next sample */
+  delay(500);
+}
